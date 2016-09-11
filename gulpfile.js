bfcdfe6b38f5c60d29bad71acdeb498241350058
Dmitry Kabanov <Dkabanov6@gmail.com>
2016-09-11 09:15:53 +0500

    Сделал начальную структуру проекта

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// Зависимости проекта
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const server = require('browser-sync');
+
+// Компиляция SASS
+gulp.task('sass', function() {
+  gulp.src('sass/style.scss')
+  .pipe(sass())
+  .pipe(gulp.dest('css'))
+  .pipe(server.reload({stream: true}));
+});
+
+// Локальный сервер, слежение
+gulp.task('serv', ['sass'], function() {
+  server.init({
+    server: '.'
+  });
+  gulp.watch('sass/**/*.scss', ['sass']);
+  gulp.watch('*.html').on('change', server.reload);
+});
